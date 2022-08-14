d46aa8a9b83b9f179f23f8874cb3d3187c0101b1
mdatikHasan <atikh2030@gmail.com>
2022-08-14 08:09:59 +0600

    2nd commit

--- js/index.js
+++ js/index.js
@@ -2,25 +2,46 @@ document.getElementById('search-btn').addEventListener('click', function () {
     const searchFieldText = document.getElementById('search-field')
 
     const searchField = searchFieldText.value;
-    // searchFieldText.value = '';
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayData(data.data))
+
+    const emptySearch = document.getElementById('emtry-search');
+    const notFound = document.getElementById('not-found');
+    if (searchFieldText.value == '') {
+
+        emptySearch.style.display = 'block'
+    } else {
+        emptySearch.style.display = 'none'
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if(data.data == false){
+                    notFound.style.display = 'block'
+                }else{
+                    displayData(data.data);
+                    notFound.style.display = 'none'
+                }
+                
+            })
+    }
+
+    searchFieldText.value = '';
+
 
 
 
 
 })
 
+
+
 const displayData = mobiles => {
     const cards = document.getElementById('cards')
-    // cards.textContent = '';
-
-    mobiles.forEach(mobile => {
+    cards.textContent = '';
+  
+    mobiles.slice(0, 20).forEach(mobile => {
         console.log(mobile)
-        const { brand, image, phone_name, slug } = mobile;
+        const { brand, image, phone_name} = mobile;
 
         const divCol = document.createElement('div');
         divCol.classList.add('col-lg-4')
@@ -31,7 +52,7 @@ const displayData = mobiles => {
                 <h5 class="card-title">${phone_name}</h5>
                 <h6 class="card-title">${brand}</h6>
             </div>
-            <input type="button" onclick="loadMobileDetail(${mobile?.slug})"  value="details">
+            <input type="button" class="btn btn-success" onclick="loadMobileDetail('${mobile?.slug}')"  value="details">
         </div>
     `;
         cards.appendChild(divCol)
@@ -40,8 +61,63 @@ const displayData = mobiles => {
 
 }
 
-
 const loadMobileDetail = phoneName => {
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneName}`
-    console.log(url)
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneName}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMobileDetail(data.data))
 }
+
+
+const displayMobileDetail = mobile => {
+    const mobileDetails = document.getElementById('mobile-details');
+    mobileDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.classList.add('mb-3')
+    div.classList.add('text-center')
+    div.classList.add('w-50')
+    div.classList.add('mx-auto')
+    div.innerHTML = `
+            <div class="row g-0 p-4">
+                <div class="col-md-4">
+                    <img src="${mobile?.image}" class="img-fluid w-100 rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${mobile?.name}</h5>
+                        <p class="card-text"><b>Main Features</b>: 
+                            <span class="card-text">${mobile?.mainFeatures?.chipSet}</span>
+                            <span class="card-text">${mobile?.mainFeatures?.displaySize}</span>
+                            <span class="card-text">${mobile?.mainFeatures?.memory}</span>
+                        </p>
+                        
+                        <p class="card-text"><b>Sensors:</b> 
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[0]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[1]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[2]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[3]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[4]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[5]}</span>,
+                            <span class="card-text">${mobile?.mainFeatures?.sensors[6]}</span>,
+                        </p>
+                        <p class="card-text"><b>Storage:</b> 
+                            <span class="card-text">${mobile?.mainFeatures?.storage}</span>,
+                        </p>
+                        <p class="card-text"><b>Others:</b> 
+                            <span class="card-text">${mobile?.others?.Bluetooth}</span>,
+                            <span class="card-text">${mobile?.others?.GPS}</span>,
+                            <span class="card-text">${mobile?.others?.NFC}</span>,
+                            <span class="card-text">${mobile?.others?.Radio}</span>,
+                            <span class="card-text">${mobile?.others?.USB}</span>,
+                            <span class="card-text">${mobile?.others?.WLAN}</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+    `;
+
+    mobileDetails.appendChild(div);
+}
+
